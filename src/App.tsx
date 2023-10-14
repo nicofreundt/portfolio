888ca089f2a19e29be5df3465d40ae8470b6cc7d
Nico Freundt <nico.freundt@protonmail.com>
2023-10-14 18:14:59 +0200

    feature: add animated background

--- src/App.tsx
+++ src/App.tsx
@@ -34,9 +34,9 @@ function App() {
 
     return (
         <>
-            <header className="absolute lg:fixed top-0 bg-black text-gray-200 w-full p-5 flex justify-between items-center">
+            <header className="absolute lg:fixed top-0 text-gray-200 w-full p-5 flex justify-between items-center">
                 <p className="text-xl font-bold">Nico Freundt</p>
-                <nav className="sr-only lg:not-sr-only bg-black text-white w-full p-5 gap-5 flex justify-around items-center">
+                <nav className="sr-only lg:not-sr-only text-white w-full p-5 gap-5 flex justify-around items-center">
                     <a className={['home', ''].includes(activeSection) ? 'text-cyan-400' : ''} href="#">Home</a>
                     <a className={activeSection === 'work' ? 'text-cyan-400' : ''} href="#work">Work</a>
                     <a className={activeSection === 'education' ? 'text-cyan-400' : ''} href="#education">Education</a>
@@ -44,14 +44,14 @@ function App() {
                     <a className={activeSection === 'certifications' ? 'text-cyan-400' : ''} href="#certifications">Certifications</a>
                 </nav>
             </header>
-            <nav className="fixed bottom-0 bg-black text-white border-t-[0.01px] border-cyan-400 border-opacity-25 w-full p-5 flex justify-around items-center lg:sr-only">
+            <nav className="fixed bottom-0 text-white border-t-[0.01px] border-cyan-400 border-opacity-25 w-full p-5 flex justify-around items-center lg:sr-only">
                 <a className={['home'].includes(activeSection) ? 'text-cyan-400' : ''} href="#"><FontAwesomeIcon icon={faHome} size="xl"/></a>
                 <a className={activeSection === 'work' ? 'text-cyan-400' : ''} href="#work"><FontAwesomeIcon icon={faBriefcase} size="xl"/></a>
                 <a className={activeSection === 'education' ? 'text-cyan-400' : ''} href="#education"><FontAwesomeIcon icon={faSchool} size="xl"/></a>
                 <a className={activeSection === 'projects' ? 'text-cyan-400' : ''} href="#projects"><FontAwesomeIcon icon={faLaptopCode} size="xl"/></a>
                 <a className={activeSection === 'certifications' ? 'text-cyan-400' : ''} href="#certifications"><FontAwesomeIcon icon={faAward} size="xl"/></a>
             </nav>
-            <main className="text-gray-200 bg-black">
+            <main className="text-gray-200">
                 <section data-section id="home" className="snap-start h-[100dvh] flex flex-col justify-center items-center outline-b-1">
                     <img className="w-60 mb-5 rounded-full aspect-square object-cover object-top" src="ich.jpg" />
                     <h1 className="text-2xl font-semibold">Full-Stack Web Developer</h1>
@@ -71,7 +71,7 @@ function App() {
                     <h1 className="text-2xl font-semibold">Projects</h1>
                 </section>
             </main>
-            <footer data-section id="certifications" className="snap-start h-[100dvh] flex justify-center items-center bg-black text-gray-200">
+            <footer data-section id="certifications" className="snap-start h-[100dvh] flex justify-center items-center text-gray-200">
                 <h1 className="text-2xl font-semibold">Certifications</h1>
             </footer>
         </>
